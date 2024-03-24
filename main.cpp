2d21cf0d997fa60d56b623a076aaf4d221566cb4
evening star <1912129881@qq.com>
2024-03-24 10:43:25 +0800

    修改sort算法为快排算法

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[],int i,int j)
+{
+	int val = arr[i];
+	int l = j;
+	int r = j;
+	while(l<r)
+	{
+		while(l < r && arr[r] >= val)
+		{
+			r--;
+		}
+		if(l < r)
+		{
+			arr[l++] = arr[r];
+		}
+		while(l < r && arr[l] <= val)
+		{
+			l++;
+		}
+		if(l < r)
+		{
+			arr[r--] = arr[l];
+		}
+	}
+	arr[l] = val;
+	sort(arr,i,l-1);
+	sort(arr,i+1,j);
+}
+
 //peng给sort函数添加注释
 void sort(int arr[], int size)
 {
-	int tmp = 0;
-	for(int i = 0; i < size - 1; i++)
-	{
-		for(int j = 0; j < size -1 -i; j++)
-		{
-			if(arr[j] > arr[j+1])
-			{
-				tmp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = tmp;
-			}
-		}
-	}
+	sort(arr,0,size);
 }
 
 int main(int argc, char** argv)
@@ -25,6 +42,11 @@ int main(int argc, char** argv)
 	int arr[] = { 12,4,89,43,21,78 };
 	int size = sizeof(arr) / sizeof(arr[0]);
 	sort(arr, size);
+	for(int i=0;i<size;++i)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout << endl;
 
 	return 0;
 }
